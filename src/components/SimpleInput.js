b297e7322781494e37633eb7976b4924dc7207bc
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-07 13:23:15 +0200

    invalid class added if enteredNameIsValid is false

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -29,9 +29,13 @@ const SimpleInput = (props) => {
     setEnteredName("");
   };
 
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
