451b5730142db028ea3e8761f8fae9b8e3d646b2
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-07 12:42:43 +0200

    Handling user input and submitting form using two approaches

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const handleNameInputChange = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    // Direct DOM manipulation should be avoided!!
+    // nameInputRef.current.value = '';
+
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={handleSubmit}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={handleNameInputChange}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
